f50942f0d90836ee5f9e709e6dca9578a21877ea
Rezwoan <frezwoan@gmail.com>
2026-05-20 00:54:54 +0600

    Add capitalize_words utility

--- pyutils/strings.py
+++ pyutils/strings.py
@@ -9,3 +9,7 @@ def is_palindrome(s):
 
 def count_vowels(s):
     return sum(1 for c in s.lower() if c in "aeiou")
+
+
+def capitalize_words(s):
+    return " ".join(word.capitalize() for word in s.split())
