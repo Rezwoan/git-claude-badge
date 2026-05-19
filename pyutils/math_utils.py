02b74ecec51135398be521c412b41d1ccc28cd22
Rezwoan <frezwoan@gmail.com>
2026-05-20 00:57:06 +0600

    Add fibonacci utility

--- pyutils/math_utils.py
+++ pyutils/math_utils.py
@@ -9,3 +9,14 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
